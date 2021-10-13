cd064e0e7faf917e870a98f9b29dbf073d257bb9
Oscar <oscar.oy@hotmail.com>
2021-10-13 13:52:07 +0200

    Doing a test using nose library

---
+++ Test.py
@@ -0,0 +1,25 @@
+'''
+This is just an example of doing our own test
+necessary to install nose: pip install nose
+Then you should choose the interpreter where packets were installed: F1 -> select interpreter
+'''
+
+def solution (num1,num2):
+    return num1+num2
+
+from nose.tools import assert_equal
+
+
+class SolutionTest(object):
+
+    def test(self,sol):
+        assert_equal(sol(2,2),4)
+        assert_equal(sol(4,4),8)
+        
+
+        print("ALL TEST CASES PASSED")
+
+
+# Run Test
+t=SolutionTest()
+t.test(solution)
