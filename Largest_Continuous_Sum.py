b4c87b12feaa4101206d928d9ccb0a822a0616a4
Oscar <oscar.oy@hotmail.com>
2021-10-21 12:13:11 +0200

    Course solution

--- Largest_Continuous_Sum.py
+++ Largest_Continuous_Sum.py
@@ -2,6 +2,7 @@
 Problem
 Given an array of integers (positive and negative) find the largest continuous sum.
 '''
+'''
 #My solution
 
 def large_cont_sum(arr):
@@ -27,8 +28,21 @@ def large_cont_sum(arr):
         print (t)
                 
     return sol
+'''
 
+#Course solution
+def large_cont_sum(arr):
 
+    if len(arr)==0:
+        return 0 
+    
+    max_sum = current_sum = arr[0]
+
+    for num in arr[1:]:
+
+        current_sum = max(current_sum + num, num)
+        max_sum = max(current_sum,max_sum)
+    return max_sum
 
 
 
@@ -38,6 +52,8 @@ def large_cont_sum(arr):
 
 print(large_cont_sum([1,2,-1,3,4,10,10,-10,-1]))
 #print(large_cont_sum([-1,1]))
+print(large_cont_sum([3,1,-1,-12,20,1,-12]))
+
 
 from nose.tools import assert_equal
 
