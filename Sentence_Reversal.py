cd36c4597c3470a731af469995eef379301dde08
Oscar <oscar.oy@hotmail.com>
2021-10-22 12:48:21 +0200

    Diferent solutions for the same problem

--- Sentence_Reversal.py
+++ Sentence_Reversal.py
@@ -18,6 +18,8 @@ both become:
 
 'here space'
 '''
+'''
+#My solution
 
 def rev_word(s):
     s= s.split()
@@ -27,6 +29,34 @@ def rev_word(s):
         sol.append(s[i])
         i-=1
     return " ".join(sol)
+
+'''
+
+#Course solution using python habilities
+def rev_word1(s):
+    return " ".join(reversed(s.split()))
+
+#Or
+
+def rev_word2(s):
+    return " ".join(s.split()[::-1])
+
+
+def rev_word(s):
+    words= []
+    length = len(s)
+    spaces = [' ']
+    i = 0
+
+    while i < length:
+        if s[i] not in spaces:
+            word_start = i
+            while i < length and s[i] not in spaces:
+                i+=1
+            words.append(s[word_start:i])
+        i+=1
+    return " ".join(reversed(words))
+
 
 print(rev_word('Hi John,   are you ready to go?'))
 print(rev_word('    space before'))
