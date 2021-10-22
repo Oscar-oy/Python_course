0a2077d3c2e875a6dfb2fb13957daedf89a463d3
Oscar <oscar.oy@hotmail.com>
2021-10-22 13:45:07 +0200

    Course solution for String Compression problem

--- String_Compression.py
+++ String_Compression.py
@@ -4,7 +4,7 @@ Given a string in the form 'AAAABBBBCCCCCDDEEEE' compress it to become 'A4B4C5D2
 
 The function should also be case sensitive, so that a string 'AAAaaa' returns 'A3a3'.
 '''
-
+'''
 #My solution
 
 import collections
@@ -22,7 +22,50 @@ def compress(s):
         i+=1
     sol = "".join(sol)
     return sol
+'''
 
+#course solution
+def compress(s):
+
+    """
+    This solution compresses without checking. Known as the RunLength Compression algorithm.
+    """
+    # Begin Run as empty string
+    r = ""
+    l = len(s)
+
+    # Check for length 0
+    if l == 0:
+        return ""
+
+    # Check for length 1
+    if l == 1:
+        return s+"1"
+
+    #Intialize Values
+    last = s[0]
+    cnt = 1
+    i = 1
+
+    while i < l:
+
+        # Check to see if it is the same letter
+        if s[i] == s[i-1]:
+            # Add a count if same as previous
+            cnt +=1
+        else:
+            # Otherwise store the previous data
+            r = r+s[i-1]+str(cnt)
+            cnt = 1
+
+        # Add to index count to terminate while loo
+        i +=1
+
+    # Put everything back into run
+    r = r+s[i-1]+str(cnt)
+
+    return r
+    
 
 print(compress('AAAAABBBBCCCC'))
 
