40c0f7fc27b63c5ceeec5d8dd3b5878c76aa44d2
Oscar <oscar.oy@hotmail.com>
2021-10-22 14:08:18 +0200

    Course solutions for Unique Characters in String

--- Unique_Characters_in_String.py
+++ Unique_Characters_in_String.py
@@ -2,6 +2,7 @@
 Problem
 Given a string,determine if it is compreised of all unique characters. For example, the string 'abcde' has all unique characters and should return True. The string 'aabcde' contains duplicate characters and should return false.
 '''
+'''
 #My solution
 def uni_char(s):
     sol = []
@@ -18,6 +19,31 @@ def uni_char(s):
 
         i+=1
     return True
+'''
+
+#Course solutions
+def uni_char(s):
+    return len(set(s)) == len(s)
+
+def uni_char2(s):
+    chars = set()
+    for let in s:
+        # Check if in set
+        if let in chars:
+            return False
+        else:
+            #Add it to the set
+            chars.add(let)
+
+    return True
+
+
+
+
+
+
+
+
 
 """
 RUN THIS CELL TO TEST YOUR CODE>
