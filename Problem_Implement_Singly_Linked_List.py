598636c86998ca88a1cca2e0dfff498cc57c5a2b
Oscar <oscar.oy@hotmail.com>
2021-10-28 19:36:54 +0200

    Problem implement a Singly and Doubly Linked List

---
+++ Problem_Implement_Singly_Linked_List.py
@@ -0,0 +1,14 @@
+class Node(object):
+
+    def __init__(self,value):
+        self.value = value
+        self.nextnode = None
+
+
+
+class DoublyNode(object):
+
+    def __init__(self,value):
+        self.value = value
+        self.next_node = None
+        self.prev_node = None
