1b79b98439f57f4ff67c0b700925cc9553f66d79
Oscar <oscar.oy@hotmail.com>
2021-10-30 00:59:16 +0200

    Course solution for the problem 2 of recursion

--- Recursion_Problem_2.py
+++ Recursion_Problem_2.py
@@ -5,7 +5,7 @@ Given an integer, create a function which returns the sum of all the individual 
 4321%10
 4321 / 10
 '''
-
+#My solution
 #print(3%10)
 def sum_func(n):
 
@@ -17,3 +17,13 @@ def sum_func(n):
     
 
 print(sum_func(4321))
+
+#Course solution
+def sum_func1(n):
+    #Base case
+    if len(str(n)) == 1:
+        return n
+    else:
+        return int(n%10)+sum_func1(int(n/10))
+
+print(sum_func1(4321))
