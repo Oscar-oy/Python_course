165bea9386ec9457a788ab7cf5877cc06f8d1e74
Oscar <oscar.oy@hotmail.com>
2021-10-30 01:12:37 +0200

    Course solution for the problem 3 of recursion

--- Recursion_Problem_3.py
+++ Recursion_Problem_3.py
@@ -10,7 +10,7 @@ word_split('themanran',['the','ran','man'])
 word_split('ilovedogsJohn',['i','am','a','dogs','lover','love','John'])
 ['i', 'love', 'dogs', 'John']
 '''
-
+#My solution
 def word_split(phrase,list_of_words,output=None):
     palabra = ""
     if output is None:
@@ -27,3 +27,32 @@ def word_split(phrase,list_of_words,output=None):
 print(word_split('themanran',['the','ran','man']))
 print(word_split('ilovedogsJohn',['i','am','a','dogs','lover','love','John']))
 print(word_split('themanran',['clown','ran','man']))
+
+#Course solution
+def word_split1(phrase,list_of_words,output = None):
+    '''
+    Note: This is a very "python-y" solution.
+    ''' 
+
+    #Checks to see if any output has been initiated
+    #If you default output = [], it would be overwritten for every recursion
+    if output is None:
+        output = []
+
+    #For every word in list
+    for word in list_of_words:
+        #If the current phrase begins with the word, we have a split point!
+        if phrase.startswith(word):
+
+            #Add the word to the output
+            output.append(word)
+
+            #Recursively call the split function on the remaining portion of the phrase[len(word)]
+            return word_split(phrase[len(word):],list_of_words,output)
+
+    #Finaly return output if no phrase.startswith(word) returns True
+    return output
+
+print(word_split1('themanran',['the','ran','man']))
+print(word_split1('ilovedogsJohn',['i','am','a','dogs','lover','love','John']))
+print(word_split1('themanran',['clown','ran','man']))
