d73f415ffee6a34821c3416f0e4b9cdfd4b8bf3f
Oscar <oscar.oy@hotmail.com>
2021-11-01 01:24:49 +0100

    Solution for the problem Reverse String

--- Problem_Reverse_String.py
+++ Problem_Reverse_String.py
@@ -6,4 +6,35 @@ Again, make sure you use recursion to accomplish this. Do not slice (e.g. string
 '''
 
 def reverse(s):
-    pass
+    
+    #Base Case
+    if len(s) <= 1:
+        return s
+    
+    #Recursion
+    return reverse(s[1:]) + s[0]
+
+print(reverse('abc'))
+
+
+'''
+RUN THIS CELL TO TEST YOUR FUNCTION AGAINST SOME TEST CASES
+'''
+
+from nose.tools import assert_equal
+
+class TestReverse(object):
+    
+    def test_rev(self,solution):
+        assert_equal(solution('hello'),'olleh')
+        assert_equal(solution('hello world'),'dlrow olleh')
+        assert_equal(solution('123456789'),'987654321')
+        
+        print ('PASSED ALL TEST CASES!')
+        
+# Run Tests
+test = TestReverse()
+test.test_rev(reverse)
+
+
+
