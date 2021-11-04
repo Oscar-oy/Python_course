b4d0dcec2e4e7c682cd2fd39ed11b81b63f96ca5
Oscar <oscar.oy@hotmail.com>
2021-11-04 13:50:17 +0100

    Course solution for coin change problem with dynamic solution

--- Problem_Coin_Change_Problem.py
+++ Problem_Coin_Change_Problem.py
@@ -39,6 +39,74 @@ def rec_coin(target,coins):
 print(rec_coin(10,[1,5]))
 
 
+# Course solution
+
+# This is a basic recursion solution which is not efficient 
+# And we could have some problems with weird numbers
+def rec_coin1(target,coins):
+
+    # DEFAULT VALUE SET TO TARGET
+    min_coins = target
+
+    # CHECKS TO SEE IF TARGET IN IN COINS VALUE LIST
+    if target in coins:
+        return 1
+
+    else:
+        # For every coin value that is <= my target
+        for i in [c for c in coins if c<=target]:
+
+            #ADD A COIN COUNT + RECURSIVE CALL
+            num_coins = 1 + rec_coin(target-i,coins)
+
+            # RESET MINIMUM IF THE NEW NUM_COINS LESS THAN MIN_COINS
+            if num_coins < min_coins:
+                min_coins = num_coins
+
+    return min_coins
+
+print(rec_coin1(63,[1,5,10,25]))
+
+# We should use memoization
+
+# Dynamicly
+def rec_coin_dynam(target,coins,know_results):
+
+    # Default output to target
+    min_coins = target
+
+    #Base Case
+    if target in coins:
+        know_results[target] = 1
+        return 1
+    # Return a known resurl if it happens to be grater than 1
+    elif know_results[target]>0:
+        return know_results[target]
+
+    else:
+
+        #For every coin value that is <=target
+        for i in [c for c in coins if c<= target]:
+
+            num_coins = 1 +rec_coin_dynam(target-i,coins,know_results)
+
+            if num_coins < min_coins:
+
+                min_coins = num_coins
+
+                # Reset that known results
+                know_results[target] = min_coins
+
+    return min_coins
+
+target = 74
+coins = [1,5,10,25]
+known_results = [0]*(target+1)
+
+print(rec_coin_dynam(target,coins,known_results))
+
+
+
 
 """
 RUN THIS CELL TO TEST YOUR FUNCTION.
