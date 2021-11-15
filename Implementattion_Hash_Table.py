84efc9c01b741bf3f2c47e09e121673bf9d59f80
Oscar <oscar.oy@hotmail.com>
2021-11-15 13:05:12 +0100

    Implementing a Hash Table but with a hash function called folding a another rehash

--- Implementattion_Hash_Table.py
+++ Implementattion_Hash_Table.py
@@ -14,6 +14,9 @@ in the map in Return True for a statement of the form key in map, if the given k
 '''
 
 
+from typing import overload
+
+
 class HashTable(object):
 
     def __init__(self,size):
@@ -111,4 +114,123 @@ print(h[0])
 '''
 diferent calls to see how it works
 '''
+
+
+'''
+Till now we used remainder method
+Now I'm going to implement diferent ways of hash function
+'''
+
+#Folding Method
+
+class FoldingHashFunction(object):
+
+    def __init__(self,size):
+
+        self.size = size
+        self.slots = [None]*self.size
+        self.data = [None]*self.size
+
+    def put(self,key,data):
+
+        hashvalue = self.hashfunction(key,len(self.slots))
+
+        if self.slots[hashvalue] == None:
+            self.slots[hashvalue] = key
+            self.data[hashvalue] = data
+
+        elif self.slots[hashvalue] == key:
+            self.data[hashvalue] = data
+
+        else:
+            pos = 1
+            nextslot = self.rehash(hashvalue,len(self.slots),pos)
+            while self.slots[nextslot] != None and self.slots[nextslot] != key:
+                pos += 2
+                nextslot = self.rehash(nextslot,len(self.slots), pos)
+
+            if self.slots[nextslot] == None:
+                self.slots[nextslot] = key
+                self.data[nextslot] = data
+            else:
+                self.data[nextslot] = data
+
+
+    def hashfunction(self,key,size):
+        #Folding Method
+        
+        def suma (key):
+            newk = [a for a in str(key)]
+        
+            k=''
+            if len(newk)>2:
+                n =''
+                for num in newk[2:]:
+                    n += num
+            else: return 0
+            
+            for num in newk[:2]:
+                k += num
+            
+
+            key = int(float(n))
+            newk = int(float(k))
+            return newk + suma(key)
+
+        return suma(key)%size 
+
+    def rehash(self,oldhash,size,pos):
+        #quadratic probing
+        return (oldhash + pos)%size
+
+
+    def get (self,key):
+        startslot = self.hashfunction(key,len(self.slots))
+        data = None
+        stop = False
+        found = False
+        position = startslot
+        pos = -1
+
+
+        while self.slots[position] != None and not found and not stop:
+
+            if self.slots[position] == key:
+                found = True
+                data = self.data[position]
+
+            else:
+                pos += 2
+                position = self.rehash(position,len(self.slots),pos)
+
+                if position == startslot:
+                    stop = True
+
+            return data
+
+
+    def __getitem__(self,key):
+        return self.get(key)
+
+    def __setitem__(self,key,data):
+        return self.put(key,data)
+
+
+hf = FoldingHashFunction(11)
+
+hf[4365554601] = 'holi'
+hf[606002845] = 'other'
+
+print(hf[4365554601])
+print(hf[606002845])
+
+
+'''
+I implemented a Hash table using folding method to hash 
+and quadrating probing to rehash
+
+'''
+
+
+
 
