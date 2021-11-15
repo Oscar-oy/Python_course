06a2df099e8a6cedbac4a7e89f7b4ab6c1d0ef31
Oscar <oscar.oy@hotmail.com>
2021-11-15 19:09:58 +0100

    Implementation of Shell Sort

---
+++ Implementation_Shell_Sort.py
@@ -0,0 +1,35 @@
+# Shell Sort
+
+def shell_sort(arr):
+
+    sublistcount = len(arr)//2
+
+    while sublistcount >0:
+        for start in range(sublistcount):
+
+            gap_insertion_sort(arr,start,sublistcount)
+
+        print ('After increments of size: ',sublistcount)
+        print ('the list is: ',arr)
+
+
+        sublistcount = sublistcount//2
+
+def gap_insertion_sort(arr,start,gap):
+
+    print(list(range(start+gap, len(arr),gap)))
+    for i in range(start+gap, len(arr),gap):
+        
+        currentvalue = arr[i]
+        position = i
+
+        while position >= gap and arr[position-gap]>currentvalue:
+            arr[position] = arr[position-gap]
+            position = position-gap
+
+        arr[position] = currentvalue
+
+
+arr = [45,67,23,45,21,24,7,2,6,4,90]
+shell_sort(arr)
+print(arr)
