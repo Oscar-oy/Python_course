d58f7c7757743f4047087b982c6ca7e834df7937
Oscar <oscar.oy@hotmail.com>
2021-11-18 11:53:37 +0100

    Implementing Merge Sort Problem

---
+++ 7 Searching and Sorting/Problem_Merge_Sort.py
@@ -0,0 +1,45 @@
+# Implementation Merge Sort
+
+
+def merge_sort(arr):
+    if len(arr)>1:
+        mid = len(arr)//2
+        left = arr[:mid]
+        right = arr[mid:]
+
+        merge_sort(left)
+        merge_sort(right)
+
+        i = 0 
+        j = 0
+        k = 0
+
+        while i < len(left) and j < len(right):
+
+            if left[i] < right[j]:
+                arr[k]= left[i]
+                i += 1
+            else:
+                arr[k] = right[j]
+                j += 1
+
+            k += 1
+
+
+        while i < len(left):
+            arr[k] = left[i]
+            i += 1
+            k += 1
+
+        while j < len(right):
+            arr[k] = right[j]
+            j += 1
+            k += 1
+
+
+arr = [11,2,5,4,7,6,8,1,23]
+merge_sort(arr)
+print(arr)
+    
+
+
