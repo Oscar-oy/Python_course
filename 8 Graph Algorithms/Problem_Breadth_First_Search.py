2abcc449f5024e8cfd80343580eac3d215689ffe
Oscar <oscar.oy@hotmail.com>
2021-11-22 18:54:42 +0100

    Breadth First Search Problem solution

---
+++ 8 Graph Algorithms/Problem_Breadth_First_Search.py
@@ -0,0 +1,50 @@
+# Implementation of Breadth First Search
+
+graph = {'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B', 'F']),
+         'F': set(['C', 'E'])}
+
+
+def bfs(graph,start):
+    visited = set()
+    queue = [start]
+    while queue:
+        vertex = queue.pop(0)
+        if vertex not in visited:
+            visited.add(vertex)
+            queue.extend(graph[vertex]-visited)
+
+    return visited
+
+print(bfs(graph, 'A'))
+
+#Paths
+
+def bfs_paths(graph,start,goal):
+    queue = [(start, [start])]
+
+    while queue:
+        (vertex,path) = queue.pop(0)
+        for next in graph[vertex] - set(path):
+            if next == goal:
+                yield path + [next]
+
+            else:
+                queue.append((next, path + [next]))
+
+print(list(bfs_paths(graph, 'A','F')))
+
+for sol in bfs_paths(graph, 'A','F'):
+    print (sol)
+
+
+def shortest_path(graph, start, goal):
+    try:
+        return next(bfs_paths(graph, start, goal))
+    except StopIteration:
+        return None
+
+print(shortest_path(graph, 'A', 'F'))
