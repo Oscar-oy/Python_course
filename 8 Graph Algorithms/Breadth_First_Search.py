86e51d13a791912b1b0590f0a604da830c5cd87a
Oscar <oscar.oy@hotmail.com>
2021-11-22 13:13:40 +0100

    Breadth First Search Implementation

---
+++ 8 Graph Algorithms/Breadth_First_Search.py
@@ -0,0 +1,55 @@
+# Implementing BFS
+
+graph = {'A': set(['B', 'C']),
+         'B': set(['A', 'D', 'E']),
+         'C': set(['A', 'F']),
+         'D': set(['B']),
+         'E': set(['B', 'F']),
+         'F': set(['C', 'E'])}
+
+
+def bfs(graph, start):
+    visited = set()
+    queue = [start]
+
+    while queue:
+        vertex = queue.pop(0)
+
+        if vertex not in visited:
+            visited.add(vertex)
+            queue.extend(graph[vertex]-visited)
+
+    return visited
+
+
+print(bfs(graph, 'A'))
+
+
+# Paths
+'''
+This implementation can again be altered slightly to instead return all possible paths between two vertices, the first of which being one of the shortest such path.
+'''
+
+def bfs_paths(graph, start, goal):
+    queue = [(start, [start])]
+    while queue:
+        (vertex, path) = queue.pop(0)
+        for next in graph[vertex]- set(path):
+            if next == goal:
+                yield path + [next]
+            else:
+                queue.append((next, path + [next]))
+
+print(list(bfs_paths(graph,'A','F')))
+
+
+def shortest_path(graph, start, goal):
+    try:
+        return next(bfs_paths(graph,start,goal))
+    except StopIteration:
+        return None
+
+print(shortest_path(graph, 'A','F'))
+
+
+
